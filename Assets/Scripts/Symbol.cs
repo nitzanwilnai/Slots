b698f1bf498c250a940c44fb6dd9143252abd624
Nitzan Wilnai <nitzan@gmail.com>
2022-07-16 15:07:19 +0300

    namespace

--- Assets/Scripts/Symbol.cs
+++ Assets/Scripts/Symbol.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Symbol : MonoBehaviour
+namespace SlotsGame
 {
-    public TextMeshPro Text;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Symbol : MonoBehaviour
     {
-        
+        public TextMeshPro Text;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
     }
 }
